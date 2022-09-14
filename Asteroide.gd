3a707258e2d7038c03b7ac5b88fb7fa130670511
centrekepler <videojocs@centrekepler.com>
2022-09-14 15:27:37 +0200

    Inicial

---
+++ Asteroide.gd
@@ -0,0 +1,6 @@
+extends Area2D
+var direccio = Vector2.RIGHT
+
+
+func _ready():
+	
---
+++ Ñau.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+var velocitat = 130
+var direccio = Vector2.UP
+var vel_gir = 4
+
+func _ready():
+	pass
+
+
+func _process(delta):
+
+	if Input.is_action_pressed("Avañça"):
+		direccio =  Vector2.UP.rotated(rotation)
+		position = global_position + direccio * velocitat * delta
+	if Input.is_action_pressed("Gir esquerre"):
+		rotate(-vel_gir * delta)
+	if Input.is_action_pressed("Gir Dret"):
+		rotate(vel_gir * delta)
+	move_and_slide(direccio)
